7fbf56b9a7df69f7255f512b1830b3c6dbb7aa37
Qkprahlad101 <k.prahlad101@gmail.com>
2023-06-29 04:05:31 +0530

    Added dynamic item addition using TextField.

--- app/src/main/java/com/example/jetpacklist/MainActivity.kt
+++ app/src/main/java/com/example/jetpacklist/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,20 +31,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val greetingListState = remember { mutableStateListOf<String>("John", "Amanda") }
+    val newNameStateContent = remember { mutableStateOf("") }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        GreetingList(greetingListState) { greetingListState.add("Prahlad") }
+        GreetingList(greetingListState,
+                    { greetingListState.add(newNameStateContent.value) },
+                    newNameStateContent.value,
+                    {newInput -> newNameStateContent.value = newInput})
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingList(namesList: List<String>, buttonClick: () -> Unit) {
+fun GreetingList(namesList: List<String>,
+                 buttonClick: () -> Unit,
+                 textFieldValue: String,
+                 textFieldUpdate: (newName: String) -> Unit) {
     for (name in namesList) {
         Greeting(name)
     }
+
+    TextField(value = textFieldValue, onValueChange = textFieldUpdate)
 
     Button(onClick = buttonClick) {
         Text(text = "Add new name")
