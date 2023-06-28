1066711456192aaaf3813747dff29a9859369b67
Qkprahlad101 <k.prahlad101@gmail.com>
2023-06-29 03:47:05 +0530

    Initialised mutableStateListOf for displaying list of hardcoded items.

---
+++ app/src/main/java/com/example/jetpacklist/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.jetpacklist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val greetingListState = remember{mutableStateListOf<String>("John", "Amanda")}
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+            GreetingList(greetingListState, { greetingListState.add("Prahlad") })
+    }
+}
+
+@Composable
+fun GreetingList(namesList: List<String>, buttonClick: () -> Unit){
+    for (name in namesList) {
+        Greeting(name)
+    }
+
+    Button(onClick = buttonClick) {
+        Text(text = "Add new name")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!",
+        style = MaterialTheme.typography.displaySmall
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MainScreen()
+}
